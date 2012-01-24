91a771d0b887876aabd0edda74d2c8327fb9d7d1
Dmitry Furzenko <dimonji@gmail.com>
2012-01-24 22:10:50 +0200

    ticket:1 write view, test passed

--- src/bio/views.py
+++ src/bio/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from bio.models import Human
+from django.shortcuts import render_to_response
+
+def show_human_bio(request):
+    """
+    Simple view for human bio
+    """
+    human = Human.objects.filter(pk=1)
+    return render_to_response("human_bio.html", {'human': human})
+
